e188cc789e9642767827aeee2dfdc80300b19193
Gisele Macieira <giselefchaves@gmail.com>
2023-07-28 22:47:49 -0300

    implementação do endpoint GET/customers/:id

--- src/controllers/customerControllers.js
+++ src/controllers/customerControllers.js
@@ -1,8 +1,8 @@
 import { db } from "../database/database.connection.js";
 
+//Formato de um cliente na tabela customers {id: 1, name: 'João Alfredo', phone: '21998899222', cpf: '01234567890', birthday: '1992-10-25'}
 
 export async function getCustomers (req, res){
-    //Formato de um cliente na tabela customers {id: 1, name: 'João Alfredo', phone: '21998899222', cpf: '01234567890', birthday: '1992-10-25'}
     try{
         const customers = await db.query(`SELECT * FROM customers;`)
         return res.send(customers.rows)
@@ -10,4 +10,16 @@ export async function getCustomers (req, res){
         return res.status(500).send(err.message)
     }
 
+}
+
+export async function getCustomer (req, res){
+    const {id} = req.params
+    try{
+        const costumer = await db.query(`SELECT * FROM customers
+        WHERE id = $1`, 
+        [id])
+        return res.send(costumer.rows)
+    }catch(err){
+        return res.status(500).send(err.message)
+    }
 }
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getCustomers } from "../controllers/customerControllers.js";
+import { getCustomer, getCustomers } from "../controllers/customerControllers.js";
 
 const customerRouter = Router();
 
 customerRouter.get('/customers', getCustomers);
+customerRouter.get('/customers/:id', getCustomer);
 
 export default customerRouter;
